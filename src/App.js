e0e96497a012d75b814dff65d5aefd67475dc5f9
mervesnygt <merve.senyigit@outlook.com>
2024-12-30 16:47:11 +0300

    Set up routing with React Router, created Home, About, and Contact pages with basic structure and styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import { Routes, Route, Link } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello, MERVE!</h1>
+        <nav>
+          <ul className='nav-ul'>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
       </header>
     </div>
   );
